d21880a82ad31cfff2a17c91a2048187ffc02805
Qiustander <qiustander@gmail.com>
2023-04-25 10:43:02 +0800

    add

---
+++ Models/bssm_model.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+class SSModel:
+    '''
+    State space model class
+    '''
+
+    def __init__(self):
+        pass
+
+    def ssm_ulg(self):
+        pass
+
+    def bsm_ng(self):
+        pass
+
+
