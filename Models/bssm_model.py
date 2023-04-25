c519059b027e9ef99afec43f7d3021900e73dcd1
Qiustander <qiustander@gmail.com>
2023-04-25 22:49:06 +0800

    upload check, model

--- Models/bssm_model.py
+++ Models/bssm_model.py
@@ -1,18 +1,93 @@
 import numpy as np
-
+from check_argument import CheckArg
 
 class SSModel:
-    '''
-    State space model class
-    '''
+    """
+    State space model class for various kinds of state space models, including:
+    ssm_ulg: General univariate linear-Gaussian state space models
+    ssm_mlg: General multivariate linear-Gaussian state space models
+    ssm_ung General univariate Non-Gaussian state space model
+    ssm_mng: General multivariate Non-Gaussian State Space Model
+    ssm_nlg: General multivariate nonlinear Gaussian state space models
+    ssm_sde: Univariate state space model with continuous SDE dynamics
+    ssm_svm: Stochastic Volatility Model in state space model
+    bsm_lg: Basic Structural (Time Series) Model
+    bsm_ng: Non-Gaussian Basic Structural (Time Series) Model
+    ar1_lg: Univariate Gaussian model with AR(1) latent process
+    ar1_ng: Non-Gaussian model with AR(1) latent process
+    """
 
-    def __init__(self):
+    def __init__(self, input_dict):
+        """
+        Args:
+            input_dict: definition of input_arguments for different models
+        """
+        self.input_dict = input_dict
+        self.check_arg = CheckArg() # initialize the checking class
+        self.state_dim = input_dict['state_dim']
+
+    def ssm_ulg(self, y):
+        """
+        Args:
+            y (np.array): Observations as time series (or vector) of length \eqn{n}.
+            input_dict:
+            obs_mtx (np.array): System matrix Z of the observation equation. Either a vector of length m, a m x n matrix.
+            state_mtx (np.array): System matrix T of the state equation. Either a m x m matrix or a m x m x n array.
+            state_mtx_lower (np.array): Lower triangular matrix R the state equation. Either a m x k matrix or a m x k x n array.
+            prior_mean (np.array): Prior mean for the initial state as a vector of length m.
+            prior_cov (np.array): Prior covariance matrix for the initial state as m x m matrix.
+            input_state (np.array): Intercept terms \eqn{C_t} for the state equation, given as a m times 1 or m times n matrix.
+            input_obs (np.array): Intercept terms \eqn{D_t} for the observations equation, given as a scalar or vector of length n.
+            init_theta (np.array): Initial values for the unknown hyperparameters theta (i.e. unknown variables excluding latent state variables).
+            update_fn (object): A function which returns list of updated model  components given input vector theta.
+                    This function should take only one  vector argument which is used to create list with elements named as Z, T, R, a1, P1, D, C, and phi,
+                    where each element matches the dimensions of the original model. If any of these components is missing, it is assumed to be constant wrt.
+                     theta. It's best to check the internal dimensions with str(model_object) as the dimensions of input arguments can differ from the final dimensions.
+                     If any of these components is missing, it is assumed to be constant wrt. the eta.
+            prior_fn (object): A function which returns log of prior density given input vector theta.
+            noise_std (np.array): A vector of standard deviations. Either a scalar or a vector of  length n.
+            state_names (str): A character vector defining the names of the states.
+        return:
+        """
+
+        if 'state_names' not in self.input_dict:
+            state_names = [f"state {i+1}" for i in range(m)]
+            self.input_dict['state_names'] = state_names
+        if 'init_theta' not in self.input_dict:
+            init_theta = np.array([])
+            self.input_dict['init_theta'] = init_theta
+
+        #check
+
+
+    def ssm_mlg(self):
         pass
 
-    def ssm_ulg(self):
+    def ssm_ung(self):
+        pass
+
+    def ssm_mng(self):
+        pass
+
+    def ssm_nlg(self):
+        pass
+
+    def ssm_sde(self):
+        pass
+
+    def ssm_svm(self):
+        pass
+
+    def bsm_lg(self):
         pass
 
     def bsm_ng(self):
+        pass
+
+    def ar1_lg(self):
+        pass
+
+    def ar1_ng(self):
         pass
 
 
--- Models/check_argument.py
+++ Models/check_argument.py
@@ -1,26 +1,140 @@
 import numpy as np
 
-def check_y(x, multivariate=False, distribution="gaussian"):
-    if not np.isnan(x).all():
-        if multivariate:
-            if not isinstance(x, np.ndarray) or x.ndim != 2:
-                raise ValueError("Argument 'y' must be a Tensor or Numpy array")
-            if x.shape[0] < 2:
-                raise ValueError("Number of rows in 'y', i.e. number of time points, must be > 1.")
+
+class CheckArg:
+    '''
+    For checking the arguments of the ssm models.
+    '''
+
+    def __init__(self):
+        pass
+
+    def check_all_argument(self, y):
+        """
+        Args:
+            y: The response time series.
+        :return:
+        """
+        self.check_y(y, multivariate=False, distribution="gaussian")
+
+    @staticmethod
+    def check_y(x, multivariate=False, distribution="gaussian"):
+        if not np.isnan(x).all():
+            if multivariate:
+                if not isinstance(x, np.ndarray) or x.ndim != 2:
+                    raise ValueError("Argument 'y' must be a Tensor or Numpy array")
+                if x.shape[0] < 2:
+                    raise ValueError("Number of rows in 'y', i.e. number of time points, must be > 1.")
+            else:
+                if not isinstance(x, np.ndarray) or x.ndim > 1:
+                    if x.ndim == 2 and x.shape[1] == 1 and x.shape[0] < 3:
+                        x = x.flatten()
+                    else:
+                        raise ValueError("Argument 'y' must be a Tensor or Numpy array.")
+                if len(x) < 2:
+                    raise ValueError("Length of argument y, i.e. number of time points, must be > 1.")
+                if distribution != "gaussian" and (np.logical_not(np.isnan(x)) & (x < 0)).any():
+                    raise ValueError(f"Negative values not allowed for {distribution} distribution.")
+                elif distribution in ["negative binomial", "binomial", "poisson"]:
+                    finite_x = x[np.isfinite(x)]
+                    if np.any((finite_x != finite_x.astype(int))):
+                        raise ValueError(f"Non-integer values not allowed for {distribution} distribution.")
+            if np.isinf(x).any():
+                raise ValueError("Argument 'y' must contain only finite or NA values.")
+
+    @staticmethod
+    def check_period(x, n):
+        if not isinstance(x, int):
+            raise ValueError("Argument 'period' should be a single integer.")
         else:
-            if not isinstance(x, np.ndarray) or x.ndim > 1:
-                if x.ndim == 2 and x.shape[1] == 1 and x.shape[0] < 3:
-                    x = x.flatten()
-                else:
-                    raise ValueError("Argument 'y' must be a Tensor or Numpy array.")
-            if len(x) < 2:
-                raise ValueError("Length of argument y, i.e. number of time points, must be > 1.")
-            if distribution != "gaussian" and (np.logical_not(np.isnan(x)) & (x < 0)).any():
-                raise ValueError(f"Negative values not allowed for {distribution} distribution.")
-            elif distribution in ["negative binomial", "binomial", "poisson"]:
-                finite_x = x[np.isfinite(x)]
-                if (finite_x != finite_x.astype(int)).any():
-                    raise ValueError(f"Non-integer values not allowed for {distribution} distribution.")
-        if np.isinf(x).any():
-            raise ValueError("Argument 'y' must contain only finite or NA values.")
-    return x
+            if x < 3:
+                raise ValueError("Argument 'period' should be an integer larger than 2.")
+            if x >= n:
+                raise ValueError("Period should be less than the number of time points.")
+
+    @staticmethod
+    def check_distribution(x, distribution):
+        """Checks that observations are compatible with their distributions are made.
+        Args:
+            x (np.array): input time serie
+            distribution (List[str]): list of distributions
+
+        """
+        for i in range(x.shape[1]):
+            if distribution[i] != "gaussian" and (np.logical_not(np.isnan(x[:, i])) & (x[:, i] < 0)).any():
+                raise ValueError(f"Negative values not allowed for {distribution[i]} distribution.")
+            else:
+                if distribution[i] in ["negative binomial", "binomial", "poisson"]:
+                    finite_x = x[:, i][np.isfinite(x[:, i])]
+                    if np.any((finite_x != finite_x.astype(int))):
+                        raise ValueError(f"Non-integer values not allowed for {distribution[i]} distribution.")
+
+    @staticmethod
+    def check_sd(x, type, add_prefix=True):
+        if add_prefix:
+            param = f"sd_{type}"
+        else:
+            param = type
+
+        if not isinstance(x, (int, float)):
+            raise ValueError(f"Argument {param} must be numeric.")
+        if x < 0:
+            raise ValueError(f"Argument {param} must be non-negative.")
+        if np.isinf(x):
+            raise ValueError(f"Argument {param} must be finite.")
+
+    @staticmethod
+    def check_xreg(x, n):
+        if x.shape[0] not in (0, n):
+            raise ValueError("Number of rows in xreg is not equal to the length of the series y.")
+        if not np.isfinite(x).all():
+            raise ValueError("Argument xreg must contain only finite values.")
+
+    @staticmethod
+    def check_beta(x, k):
+        if not np.issubdtype(x.dtype, np.number):
+            raise ValueError("'beta' must be numeric.")
+        if len(x) != k:
+            raise ValueError("Number of coefficients in beta is not equal to the number of columns of xreg.")
+        if not np.isfinite(x).all():
+            raise ValueError("Argument 'beta' must contain only finite values.")
+
+    @staticmethod
+    def check_mu(x):
+        if len(x) != 1:
+            raise ValueError("Argument 'mu' must be of length one.")
+        if not np.isfinite(x).all():
+            raise ValueError("Argument 'mu' must contain only finite values.")
+
+    @staticmethod
+    def check_rho(x):
+        if len(x) != 1:
+            raise ValueError("Argument 'rho' must be of length one.")
+        if abs(x) >= 1:
+            raise ValueError("Argument 'rho' must be strictly between -1 and 1.")
+
+    @staticmethod
+    def check_phi(x):
+        if x < 0:
+            raise ValueError("Parameter 'phi' must be non-negative.")
+
+    @staticmethod
+    def check_u(x, y, multivariate=False):
+        if (x < 0).any():
+            raise ValueError("All values of 'u' must be non-negative.")
+        if multivariate:
+            if len(x) == 1:
+                x = np.matrix(x).reshape(np.shape(y))
+            if not (isinstance(x, np.matrix) and np.issubdtype(x.dtype, np.number)):
+                raise ValueError("Argument 'u' must be a numeric matrix or multivariate ts object.")
+            if not np.array_equal(np.shape(y), np.shape(x)):
+                raise ValueError("Dimensions of 'y' and 'u' do not match.")
+        else:
+            if len(x) == 1:
+                x = np.repeat(x, len(y))
+            if not (isinstance(x, np.ndarray) and np.issubdtype(x.dtype, np.number)):
+                raise ValueError("Argument 'u' must be a numeric vector or ts object.")
+            if len(x) != len(y):
+                raise ValueError("Lengths of 'u' and 'y' do not match.")
+        if not np.isfinite(x).all():
+            raise ValueError("Argument 'u' must contain only finite values.")
